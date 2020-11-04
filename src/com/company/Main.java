ff1b933f1d8d25c218115912152c481ff1f6a4f5
neena <nalapatt@email.com>
2020-11-04 13:35:26 -0500

    committing java class file

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.io.PrintStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String data = "Hello from the Course 2 Github Project";
+        System.out.print(data);
+    }
+}
